ccc66e1a97637802c929e552dab0704f8183f47a
Alex <89089201593@mail.ru>
2021-04-14 18:36:39 +0300

    my project first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+const numbersOffilms = +prompt('How many films are you watched?', '');
 
-const user = "Ivan";
+const personalMovieDB = {
+    count: numbersOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-alert(`Hi, ${user}`);
+const a = prompt('What the final film are you watched?'),
+    b = prompt('Your rating of film?'),
+    c = prompt('What the final film are you watched?'),
+    d = prompt('Your rating of film?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
