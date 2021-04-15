24001b253b57410d9ee14fc7fae707494da674e9
Alex <89089201593@mail.ru>
2021-04-15 13:09:43 +0300

    for()

--- js/script.js
+++ js/script.js
@@ -8,13 +8,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of the last movies you watched?'),
-    b = prompt('How much would you rate it?'),
-    c = prompt('One of the last movies you watched?'),
-    d = prompt('How much would you rate it?');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of the last movies you watched?'),
+          b = prompt('How much would you rate it?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
+if (personalMovieDB.count < 10) {
+    alert('Quite a few movies watched');
+} else if (9 < personalMovieDB.count < 30) {
+    alert('You are a classic spectator');
+} else if (personalMovieDB.count > 29) {
+    alert('You are a movie fan');
+} else {
+    alert('Error');
+}
 
 console.log(personalMovieDB);
